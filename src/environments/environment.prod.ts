7f02d37b2b1fbf5d1f9967dcbf917c042b2561ea
lucas <lucasgschons@gmail.com>
2023-04-18 19:52:48 -0300

    manutencao no codigo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyB7AzXMDEF94VwPtd8RrljquEn4S5c8c30",
+    authDomain: "merceariaschons-e4e45.firebaseapp.com",
+    projectId: "merceariaschons-e4e45",
+    storageBucket: "merceariaschons-e4e45.appspot.com",
+    messagingSenderId: "293195414973",
+    appId: "1:293195414973:web:0b108a8c974ac63e6a66d4",
+    measurementId: "G-FKZQR6WNEQ"
+  }
+  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyB7AzXMDEF94VwPtd8RrljquEn4S5c8c30",
+    authDomain: "merceariaschons-e4e45.firebaseapp.com",
+    projectId: "merceariaschons-e4e45",
+    storageBucket: "merceariaschons-e4e45.appspot.com",
+    messagingSenderId: "293195414973",
+    appId: "1:293195414973:web:0b108a8c974ac63e6a66d4",
+    measurementId: "G-FKZQR6WNEQ"
+  }
+  
 };
 
 /*
